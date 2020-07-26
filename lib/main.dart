8a60c08a4459d63023f63428a9cfe7d16f3d4e64
Mohammed Junaid <mohammed.junaid1000@gmail.com>
2020-07-26 20:24:44 +0530

    - Lesson Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,10 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  int currentQuestion = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +69,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswerAndUpdateQuestion(true);
               },
             ),
           ),
@@ -79,13 +87,49 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswerAndUpdateQuestion(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
+    );
+  }
+
+  void checkAnswerAndUpdateQuestion(bool answer) {
+    setState(() {
+      if (quizBrain.isFinished()) {
+        Alert(
+                context: context,
+                title: "Finished",
+                desc: "All questions are done! Restarting")
+            .show();
+        quizBrain.reset();
+        scoreKeeper.clear();
+        return;
+      }
+
+      addScore(answer);
+
+      quizBrain.nextQuestion();
+    });
+  }
+
+  void addScore(bool answer) {
+    if (answer == quizBrain.getAnswer()) {
+      scoreKeeper.add(getIcon(true));
+    } else {
+      scoreKeeper.add(getIcon(false));
+    }
+  }
+
+  Icon getIcon(bool isCorrect) {
+    return Icon(
+      isCorrect ? Icons.check : Icons.close,
+      color: isCorrect ? Colors.green : Colors.red,
     );
   }
 }
